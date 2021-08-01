f7f30b648d4334a8530da6c2db479c6145d4aaff
wocodes <william.odiomonafe@gmail.com>
2021-08-01 14:54:52 +0100

    Changed GetTopPeforming to GetLogs

--- routes/apis/logs.php
+++ routes/apis/logs.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::prefix("logs")->group(function() {
-    Route::get('/', '\App\Actions\Assets\Logs\GetTopPerforming')->middleware('auth:api');
+    Route::get('/', '\App\Actions\Assets\Logs\GetLogs')->middleware('auth:api');
     Route::post('/', '\App\Actions\Assets\Logs\Create')->middleware('auth:api');
     Route::get('/update', '\App\Actions\Assets\Logs\UpdateAssetLogs')->middleware('auth:api');
 });
