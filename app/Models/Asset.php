ba3d9ec8318535ffcb1b91b97ca691a84609a287
William <william.odiomonafe@gmail.com>
2021-10-17 20:25:56 +0100

    Updated new asset fetching process

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
 {
-    protected $fillable = ['name', 'symbol', 'asset_type_id'];
+    protected $fillable = ['name', 'symbol', 'asset_type_id', 'icon'];
 
     public function assetType()
     {
