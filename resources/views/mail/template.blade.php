88a7c31d186f3ae8b613268ec3a8c3bc8573aa02
William <william.odiomonafe@gmail.com>
2021-11-06 22:54:08 +0100

    Added extra column to seperate quantity bought and current quantity

--- resources/views/mail/template.blade.php
+++ resources/views/mail/template.blade.php
@@ -6,7 +6,7 @@
         }
 
         table {
-            width: 100%;
+            width: 50%;
             box-sizing:border-box;
             font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';
             margin:0 auto;
