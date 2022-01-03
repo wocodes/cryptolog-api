dafa7d5c1eff6532f66a314e39b62a2c5076cbcc
william <william.odiomonafe@gmail.com>
2022-01-03 21:24:51 +0100

    Merged real estate logging to it

--- routes/apis/logs.php
+++ routes/apis/logs.php
@@ -8,8 +8,8 @@ Route::middleware('auth:api')->prefix("logs")->group(function ($router) {
     Route::get('/', '\App\Actions\Assets\Logs\GetLogs');
     Route::post('/', '\App\Actions\Assets\Logs\CreateLog');
     $router->get('/update', function () {
-        UpdateCryptoAssetValue::handle();
-        UpdateRealEstateAssetValue::handle();
+        (new UpdateCryptoAssetValue)->handle();
+        (new UpdateRealEstateAssetValue)->handle();
     });
     $router->post('/{id}/withdrawal', '\App\Actions\Assets\Logs\CreateWithdrawal');
 });
